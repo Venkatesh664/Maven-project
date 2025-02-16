9aebb44e8850da7332fc7bed18aa7d1385fad9f4
Venkatesh664 <103093498+Venkatesh664@users.noreply.github.com>
2025-02-16 21:34:06 +0530

    MyApp.java

---
+++ MyApp.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class MyApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter your name:");
+        String name = scanner.nextLine();
+        
+        System.out.println("Hello, " + name + "! Welcome to your Maven application.");
+        
+        scanner.close();
+    }
+}
