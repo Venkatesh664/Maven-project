2dfdf6f89b38622131e5d5de3cad46e3c6eb2e7d
Venkatesh664 <103093498+Venkatesh664@users.noreply.github.com>
2025-02-16 21:34:42 +0530

    MyAppTest.java

---
+++ MyAppTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class MyAppTest {
+    @Test
+    public void testApp() {
+        assertTrue("Basic test to ensure setup is correct", true);
+    }
+}
